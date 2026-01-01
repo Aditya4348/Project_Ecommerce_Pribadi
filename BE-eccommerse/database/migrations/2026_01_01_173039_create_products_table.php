dfefab24c1e54bcb444119061d400ef08ed083cb
aditya <aditalvisa@gmail.com>
2026-01-02 00:42:37 +0700

    { ADD } migration Product And Reviews

---
+++ BE-eccommerse/database/migrations/2026_01_01_173039_create_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price');
+            $table->json('category')->nullable();
+            $table->text('images')->nullable();
+            $table->integer('stock');
+            $table->string('sku');
+            $table->boolean('featured')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::create('review', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->text('comment');
+            $table->integer('rating');
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
